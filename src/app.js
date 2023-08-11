f9c609a1e1b51e16cbe336b8fd94f5210b3948a7
Valeriia Harmash <valeriaharmash@gmail.com>
2023-08-11 13:10:01 -0400

    done

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ const app = express()
 const Restaurant = require('../models/index')
 const db = require('../db/connection')
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.get('/restaurants', async (req, res) => {
 	try {
 		const data = await Restaurant.findAll()
@@ -18,6 +21,59 @@ app.get('/restaurants/:id', async (req, res) => {
 		res.json(data)
 	} catch (err) {
 		console.error(err)
+	}
+})
+
+app.post('/restaurants', async (req, res, next) => {
+	try {
+		const restaurant = await Restaurant.create(req.body)
+
+		if (!restaurant) {
+			throw new Error('No restaurant created')
+		}
+		res.send(restaurant)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.put('/restaurants/:id', async (req, res, next) => {
+	try {
+		const updated = await Restaurant.update(req.body, {
+			where: {
+				id: req.params.id
+			}
+		})
+
+		console.log(updated)
+
+		if (updated[0] === 0) {
+			throw new Error('No update made')
+		}
+
+		res.sendStatus(200)
+	} catch (error) {
+		next(error)
+	}
+})
+
+app.delete('/restaurants/:id', async (req, res, next) => {
+	try {
+		const deleted = await Restaurant.destroy({
+			where: {
+				id: req.params.id
+			}
+		})
+
+		console.log(deleted)
+
+		if (deleted === 0) {
+			throw new Error('No restaurant deleted')
+		}
+
+		res.sendStatus(200)
+	} catch (error) {
+		next(error)
 	}
 })
 
